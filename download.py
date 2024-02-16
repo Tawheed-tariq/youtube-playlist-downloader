50bc47723fe55f6d738282020ef6b8d407ea4ac2
Tawheed tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-02-16 19:16:47 +0530

    commit

--- download.py
+++ download.py
@@ -1,10 +1,17 @@
 from pytube import YouTube
-
+import os
 
 def downloadVideo(videoUrl):
+    path = os.path.expanduser('~/tawheed/Downloads/Youtube-videos')
     try:
-        YouTube(videoUrl).streams.first().download()
-        print("downloaded video ")
+        YouTube(videoUrl).streams.get_highest_resolution().download(path)
+        # When you request .streams, Pytube returns a list of available streams
+        #  for that particular video. These streams represent different formats
+        #  and qualities in which the video is available for download. 
+        # The .first() method simply selects the first stream from this list, 
+        # typically in the highest resolution available, which is usually the 
+        # best quality stream.
+        print(f"downloaded video to {path}")        
     except:
         print("Connection Error")
 
--- main.py
+++ main.py
@@ -2,6 +2,30 @@ from playwright.sync_api import sync_playwright # for browser automation
 import argparse
 from download import downloadVideo
 
+def getVideoUrl(item, page):
+    #hover upon the video to show up the options button
+    item.locator("//div[@id='content']").hover()
+
+    #click on the options button
+    item.locator("//button[@id='button']").click()
+
+    #get all the options in option bar
+    optionsXpath = "//tp-yt-paper-listbox[@id='items']/ytd-menu-service-item-renderer"
+    options = page.locator(optionsXpath).all()
+
+    #click the last option as it is the 'share' option
+    options[-1].click()
+    page.wait_for_timeout(1000)
+
+    #select the url of video from popup window after clicking share
+    popup = page.locator("//tp-yt-paper-dialog")
+    url = popup.locator("//input[@id='share-url']").input_value()
+
+    #close the popup window
+    closeButton = "//button[@id='button'][@aria-label='Cancel']"
+    popup.locator(closeButton).click()
+    return url
+
 
 def main():
     with sync_playwright() as p:
@@ -17,28 +41,7 @@ def main():
         videoXpath = "//div[@id='contents']/ytd-playlist-video-list-renderer/div[@id='contents']/ytd-playlist-video-renderer"
         contents = page.locator(videoXpath).all()
         for item in contents[:total]:
-            #hover upon the video to show up the options button
-            item.locator("//div[@id='content']").hover()
-
-            #click on the options button
-            item.locator("//button[@id='button']").click()
-
-            #get all the options in option bar
-            optionsXpath = "//tp-yt-paper-listbox[@id='items']/ytd-menu-service-item-renderer"
-            options = page.locator(optionsXpath).all()
-
-            #click the last option as it is the 'share' option
-            options[-1].click()
-            page.wait_for_timeout(1000)
-
-            #select the url of video from popup window after clicking share
-            popup = page.locator("//tp-yt-paper-dialog")
-            url = popup.locator("//input[@id='share-url']").input_value()
-
-            #close the popup window
-            closeButton = "//button[@id='button'][@aria-label='Cancel']"
-            popup.locator(closeButton).click()
-
+            url = getVideoUrl(item, page)
             #download the video
             downloadVideo(url)
             page.wait_for_timeout(2000)
